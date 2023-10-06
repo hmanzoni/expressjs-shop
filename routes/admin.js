345f22e774f5d59807e04fe3fa3efb20e39c33ea
Hugo Manzoni <hugocmdesign@gmail.com>
2023-10-07 01:43:33 +0200

    render and send variables to the templates pug

--- routes/admin.js
+++ routes/admin.js
@@ -5,16 +5,25 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
   // res.send(
   //   '<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
   // );
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', {
+    prods: products,
+    pageTitle: 'Add product',
+    path: '/admin/add-product',
+  });
 });
 
-router.post('/product', (req, res, next) => {
-  console.log(req.body);
+router.post('/add-product', (req, res, next) => {
+  // console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
