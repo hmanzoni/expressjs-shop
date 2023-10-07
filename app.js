76e8153283c43a60e6f66481a0ffaf5c68b90c2b
Hugo Manzoni <hugocmdesign@gmail.com>
2023-10-07 02:54:26 +0200

    setup view engine for ejs

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
+// const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine(
-  'hbs',
-  expressHbs({
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout',
-    extname: 'hbs',
-  })
-);
+// app.engine('hbs',expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs', }));
+// app.set('view engine', 'hbs');
 // app.set('view engine', 'pug');
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -28,7 +22,7 @@ app.use(shopRoutes);
 app.use((req, res, next) => {
   // res.status(404).send('<h1>Page not found </h1>');
   // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '' });
 });
 
 app.listen(3000);
