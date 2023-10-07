f285f20dcde3294017212ca81badbe065c4543c5
Hugo Manzoni <hugocmdesign@gmail.com>
2023-10-07 03:35:11 +0200

    create a model for the products

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,7 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProuct = (req, res, next) => {
+  const products = Product.fetchAll();
   res.render('add-product', {
     prods: products,
     pageTitle: 'Add product',
@@ -12,11 +13,13 @@ exports.getAddProuct = (req, res, next) => {
 };
 
 exports.postAddProuct = (req, res, next) => {
-  products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
+  const products = Product.fetchAll();
   res.render('shop', {
     prods: products,
     pageTitle: 'Shop',
