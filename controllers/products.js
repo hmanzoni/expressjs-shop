d5d9c4d7bd7419985255c08ecbd926197686d7bd
Hugo Manzoni <hugocmdesign@gmail.com>
2023-10-07 04:07:26 +0200

    add logic for save the data on json

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,15 @@
 const Product = require('../models/product');
 
 exports.getAddProuct = (req, res, next) => {
-  const products = Product.fetchAll();
-  res.render('add-product', {
-    prods: products,
-    pageTitle: 'Add product',
-    path: '/admin/add-product',
-    activeProduct: true,
-    formCSS: true,
-    productCSS: true,
+  Product.fetchAll((products) => {
+    res.render('add-product', {
+      prods: products,
+      pageTitle: 'Add product',
+      path: '/admin/add-product',
+      activeProduct: true,
+      formCSS: true,
+      productCSS: true,
+    });
   });
 };
 
@@ -19,13 +20,14 @@ exports.postAddProuct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll();
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProds: products.length > 0,
-    activeShop: true,
-    productCSS: true,
+  Product.fetchAll((products) => {
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProds: products.length > 0,
+      activeShop: true,
+      productCSS: true,
+    });
   });
 };
