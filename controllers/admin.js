bff2969017e0e95f58a4d04ce078f4125b45a6ef
Hugo Manzoni <hugocmdesign@gmail.com>
2023-10-10 23:02:51 +0200

    refactor the controller

--- controllers/admin.js
+++ controllers/admin.js
@@ -9,10 +9,7 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
-  const price = req.body.price;
-  const description = req.body.description;
+  const { title, imageUrl, price, description } = req.body;
   const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect('/');
@@ -38,17 +35,13 @@ exports.getEditProduct = (req, res, next) => {
 };
 
 exports.postEditProduct = (req, res, next) => {
-  const prodId = req.body.productId;
-  const updatedTitle = req.body.title;
-  const updatedPrice = req.body.price;
-  const updatedImgUrl = req.body.imageUrl;
-  const updatedDesc = req.body.description;
+  const { productId, title, imageUrl, price, description } = req.body;
   const updatedProd = new Product(
-    prodId,
-    updatedTitle,
-    updatedImgUrl,
-    updatedDesc,
-    updatedPrice
+    productId,
+    title,
+    imageUrl,
+    price,
+    description
   );
   updatedProd.save();
   res.redirect('/admin/products');
