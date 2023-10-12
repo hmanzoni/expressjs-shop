b412585c9db75c80208fd13a375a44476399b0ea
Hugo Manzoni <hugocmdesign@gmail.com>
2023-10-12 19:22:57 +0200

    add cart relations

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -35,6 +37,10 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constrains: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
   // .sync({ force: true }) // FOR DROP AND CREATE TABLES
@@ -48,8 +54,10 @@ sequelize
     }
     return user;
   })
+  .then((user) => {
+    return user.createCart();
+  })
   .then((res) => {
-    console.log(res);
     app.listen(3000);
   })
   .catch((err) => console.log(err));
