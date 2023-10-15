8a9e75e5d27773ceaaff04ab83f451ce6a05a5a8
Hugo Manzoni <hugocmdesign@gmail.com>
2023-10-15 22:05:43 +0200

    create a model for order

---
+++ models/order.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const orderSchema = new Schema({
+  products: [
+    {
+      product: {
+        type: Object,
+        required: true,
+      },
+      quantity: { type: Number, required: true },
+    },
+  ],
+  user: {
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    name: { type: String, required: true },
+  },
+});
+
+module.exports = mongoose.model('Order', orderSchema);
