f4d8f01edd481cb7d2f010d42ac90210f6edb06f
Hugo Manzoni <hugocmdesign@gmail.com>
2023-10-21 16:10:54 +0200

    check length and alphanum password

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator/check');
+const { check, body } = require('express-validator/check');
 
 const authController = require('../controllers/auth');
 
@@ -15,15 +15,23 @@ router.get('/reset/:token', authController.getNewPass);
 
 router.post(
   '/login',
-  check('email')
-    .isEmail()
-    .withMessage('Please enter a valid email.')
-    .custom((value, { req }) => {
-      if (value === 'test@test.com') {
-        throw new Error('This email address if forbidden.');
-      }
-      return true;
-    }),
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Please enter a valid email.')
+      .custom((value, { req }) => {
+        if (value === 'test@test.com') {
+          throw new Error('This email address if forbidden.');
+        }
+        return true;
+      }),
+    body(
+      'password',
+      'Please enter a password with only numbers and text and at least 5 characters.'
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+  ],
   authController.postLogin
 );
 
