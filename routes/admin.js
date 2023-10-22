88c3dc5bfe471ab935b0d2b4fca897d19be627bd
Hugo Manzoni <hugocmdesign@gmail.com>
2023-10-23 00:19:52 +0200

    create a delete route for delete prods

--- routes/admin.js
+++ routes/admin.js
@@ -36,4 +36,6 @@ router.post(
 
 router.post('/delete-product', isAuth, adminController.postDeleteProduct);
 
+router.delete('/product/:productId', isAuth, adminController.deleteProduct);
+
 module.exports = router;
