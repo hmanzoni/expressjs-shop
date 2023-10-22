9ec490b95111c2d826ec84984be0e810296cdd93
Hugo Manzoni <hugocmdesign@gmail.com>
2023-10-22 03:09:06 +0200

    delete file from data storage when delete prod

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,6 @@
 const Product = require('../models/product');
 const { validationResult } = require('express-validator');
+const { deleteFile } = require('../util/fileHandler');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
@@ -124,6 +125,7 @@ exports.postEditProduct = (req, res, next) => {
       }
       product.title = title;
       if (imageFile) {
+        deleteFile(product.image);
         product.image = imageFile.path;
       }
       product.price = price;
@@ -163,17 +165,29 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteOne({ _id: prodId, userId: req.user._id }).then(() => {
-    req.user
-      .deleteCartItem(prodId)
-      .then(() => {
-        res.redirect('/admin/products');
-      })
-      .catch((err) => {
-        console.log(err);
-        const error = new Error(err);
-        error.httpStatusCode = 500;
-        return next(error);
-      });
-  });
+  Product.findById(prodId)
+    .then((prod) => {
+      if (!prod) {
+        return next(new Error('Product not found.'));
+      }
+      deleteFile(prod.image);
+      return Product.deleteOne({ _id: prodId, userId: req.user._id })
+        .then(() => {
+          req.user.deleteCartItem(prodId).then(() => {
+            res.redirect('/');
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          const error = new Error(err);
+          error.httpStatusCode = 500;
+          return next(error);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
